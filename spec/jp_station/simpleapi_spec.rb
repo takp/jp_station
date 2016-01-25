078327e27a32ec895b0ba90193a6081a43eb43ba
takp <takayoshi.nishida@gmail.com>
2016-01-25 14:35:18 +0700

    Fix error when response has nil

--- spec/jp_station/simpleapi_spec.rb
+++ spec/jp_station/simpleapi_spec.rb
@@ -1,19 +1,35 @@
 require 'spec_helper'
+require 'pp'
 
 describe JpStation::Simpleapi do
   context "with params" do
-    before do
-      @response = JpStation::Simpleapi.stations
+    context "when response with full data" do
+      before do
+        @response = JpStation::Simpleapi.stations
+      end
+      it 'returns a hash' do
+        expect(@response.class).to eq Hash
+      end
+      it "returns a correct count" do
+        expect(@response[:stations].length).to eq @response[:count]
+      end
+      it 'returns a correct hash' do
+        expect(@response[:count]).to eq 8
+        expect(@response[:stations].first[:name]).to eq "西新宿駅"
+      end
     end
-    it 'returns a hash' do
-      expect(@response.class).to eq Hash
-    end
-    it "returns a correct count" do
-      expect(@response[:stations].length).to eq @response[:count]
-    end
-    it 'returns a correct hash' do
-      expect(@response[:count]).to eq 8
-      expect(@response[:stations].first[:name]).to eq "西新宿駅"
+
+    context "when some response has nil data" do
+      before do
+        lat, lng = ["35.47700594420719", "139.63423721855474"]
+        @response = JpStation::Simpleapi.stations(lat, lng)
+      end
+      it 'returns a hash' do
+        expect(@response.class).to eq Hash
+      end
+      it "returns a correct count" do
+        expect(@response[:stations].length).to eq @response[:count]
+      end
     end
   end
 
